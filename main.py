0654fcc1f41b516436dab00b93976ae772fede6d
BuildTools <nikdraws@users.noreply.github.com>
2023-01-06 17:48:13 +0100

    optimized the whole bot

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
-import discord
-from discord.ext import commands
+import lightbulb
+import hikari
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
+bot = lightbulb.BotApp(token=os.getenv("TOKEN"),
+                       prefix=">>",
+                       intents=hikari.Intents.ALL,
+                       # Insert your guild id
+                       default_enabled_guilds=[990664463621439539])
 
-client = commands.Bot(command_prefix=">>", intents=discord.Intents.all())
+
+bot.load_extensions_from("extensions/", must_exist=True, recursive=True)
 
 
-@client.event
-async def on_ready():
-    print("Bot Online!")
+@bot.listen(hikari.StartedEvent)
+async def on_start(event):
+    print("Bot has started")
 
-client.run(os.getenv("TOKEN"))
+
+@bot.command
+@lightbulb.command("ping", "checks if the bot is alive")
+@lightbulb.implements(lightbulb.PrefixCommand)
+async def ping(ctx: lightbulb.Context) -> None:
+    await ctx.respond("Pong!")
+
+bot.run()

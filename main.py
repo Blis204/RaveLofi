b18ddf734513c3669f0d2ad838b7d26e17998ce0
BuildTools <nikdraws@users.noreply.github.com>
2023-01-06 11:15:58 +0100

    switched to pycord

--- main.py
+++ main.py
@@ -1,29 +1,15 @@
-import lightbulb
-import hikari
+import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 import os
-
 load_dotenv()
 
-bot = lightbulb.BotApp(token=os.getenv("TOKEN"),
-                       prefix=">>",
-                       intents=hikari.Intents.ALL,
-                       # Insert your guild id
-                       default_enabled_guilds=[1004096813906792458])
+
+client = commands.Bot(command_prefix=">>", intents=discord.Intents.all())
 
 
-bot.load_extensions_from("extensions/", must_exist=True, recursive=True)
+@client.event
+async def on_ready():
+    print("Bot Online!")
 
-
-@bot.listen(hikari.StartedEvent)
-async def on_start(event):
-    print("Bot has started")
-
-
-@bot.command
-@lightbulb.command("ping", "checks if the bot is alive")
-@lightbulb.implements(lightbulb.PrefixCommand)
-async def ping(ctx: lightbulb.Context) -> None:
-    await ctx.respond("Pong!")
-
-bot.run()
+client.run(os.getenv("TOKEN"))
